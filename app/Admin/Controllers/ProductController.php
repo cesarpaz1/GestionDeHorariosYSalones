788be059d596ee48c3a4bd72e402733e472ae174
CESAR <CESAR@DESKTOP-P3P9PA2>
2024-05-09 12:43:06 -0500

    subida de proyeto

---
+++ app/Admin/Controllers/ProductController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use OpenAdmin\Admin\Controllers\AdminController;
+use OpenAdmin\Admin\Form;
+use OpenAdmin\Admin\Grid;
+use OpenAdmin\Admin\Show;
+use \App\Models\Product;
+
+class ProductController extends AdminController
+{
+    /**
+     * Title for current resource.
+     *
+     * @var string
+     */
+    protected $title = 'Product';
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new Product());
+
+        $grid->column('id', __('Id'));
+        $grid->column('descripcion', __('Descripcion'));
+        $grid->column('precio', __('Precio'));
+        $grid->column('stock', __('Stock'));
+        $grid->column('img', __('Img'));
+        $grid->column('created_at', __('Created at'));
+        $grid->column('updated_at', __('Updated at'));
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(Product::findOrFail($id));
+
+        $show->field('id', __('Id'));
+        $show->field('descripcion', __('Descripcion'));
+        $show->field('precio', __('Precio'));
+        $show->field('stock', __('Stock'));
+        $show->field('img', __('Img'));
+        $show->field('created_at', __('Created at'));
+        $show->field('updated_at', __('Updated at'));
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new Product());
+
+        $form->text('descripcion', __('Descripcion'));
+        $form->decimal('precio', __('Precio'));
+        $form->number('stock', __('Stock'));
+        $form->image('img', __('Img'));
+
+        return $form;
+    }
+}
